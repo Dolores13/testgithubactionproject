5fba7435fe7d24c9c0f568063113a8ae6ad7738f
L00196725 <L00196725@atu.ie>
2025-10-26 17:02:54 +0000

    first commit

---
+++ testproject.py
@@ -0,0 +1,11 @@
+# testproject.py
+
+# Ask for name and age
+name = input("What's your name? ")
+age = int(input("How old are you? "))
+
+# Calculate future age
+future_age = age + 10
+
+# Display message
+print(f"Hello, {name}! In 10 years you will be {future_age} years old.")
