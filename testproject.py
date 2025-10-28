f289ae4b6d730ec6e7a7b864446bf38672b90d69
L00196725 <L00196725@atu.ie>
2025-10-28 17:29:51 +0000

    new line to create file

--- testproject.py
+++ testproject.py
@@ -8,4 +8,6 @@ age = int(input("How old are you? "))
 future_age = age + 10
 
 # Display message
-print(f"Hello, {name}! In 10 years you will be {future_age} years old.")
+with open("result.txt", "a") as file:
+    file.write(f"Hello, {name}! In 10 years you will be {future_age} years old.\n")
+
